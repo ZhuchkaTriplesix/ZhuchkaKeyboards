68fce90a99ed6265826949cb1dc2e7d4146ccd8f
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2023-11-15 17:17:10 +0600

    tasks

--- api/test.py
+++ api/test.py
@@ -328,4 +328,21 @@ def add_task():
         return jsonify(answer)
 
 
+@app.route("/services/tasks/add", methods=["POST"])
+def add_ser_task():
+    order = request.args.get("order")
+    worker = request.args.get("worker")
+    try:
+        task = TasksCrud.add_ser_task(int(order), int(worker))
+        if task is not False:
+            answer = {"status": "200", "answer": "Successful add"}
+            return jsonify(answer)
+        else:
+            answer = {"status": "400", "answer": "123"}
+            return jsonify(answer)
+    except ValueError:
+        answer = {"status": "400", "answer": "Error"}
+        return jsonify(answer)
+
+
 app.run(debug=True)
