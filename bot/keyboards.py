da871d7775afecd00c9eb7803480094277d605a6
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2023-11-22 15:35:48 +0600

    fix

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -2,8 +2,9 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardMarkup, InlineKeyboardButton)
 
 start = [
-    [KeyboardButton(text="⌨️PRODUCTS⌨️"),
-     KeyboardButton(text="🛠SERVICES🛠")]
+    [[KeyboardButton(text="⌨️PRODUCTS⌨️"),
+     KeyboardButton(text="🛠SERVICES🛠")]],
+    [[KeyboardButton(text="🔔SUPPORT🔔")]]
 ]
 start_kb = ReplyKeyboardMarkup(keyboard=start, resize_keyboard=True)
 products = [
@@ -12,10 +13,12 @@ products = [
 ]
 products_kb = InlineKeyboardMarkup(inline_keyboard=products)
 services = [
-    [InlineKeyboardButton(text="Lubing switches", callback_data="Lubing switches")]
+    [InlineKeyboardButton(text="Lubing switches", callback_data="Lubing switches")],
+    [InlineKeyboardButton(text="Назад", callback_data="Back")]
 ]
 service_kb = InlineKeyboardMarkup(inline_keyboard=services)
 kb65 = [
-    [InlineKeyboardButton(text="ZK board", callback_data="ZK board")]
+    [InlineKeyboardButton(text="ZK board", callback_data="ZK board")],
+    [InlineKeyboardButton(text="Назад", callback_data="Back_prod")]
 ]
 kb65_kb = InlineKeyboardMarkup(inline_keyboard=kb65)
