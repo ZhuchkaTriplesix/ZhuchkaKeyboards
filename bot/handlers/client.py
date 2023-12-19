38676541c57f449796a4677a666d37aa448e4548
JuntexZhuk <mrlololoshka94@gmail.com>
2023-12-20 03:08:29 +0600

    feat(initial catalog)

--- bot/handlers/client.py
+++ bot/handlers/client.py
@@ -1,11 +1,17 @@
 from aiogram import F
 from aiogram.types import Message, CallbackQuery
 from aiogram import Router
-from bot.keyboard import main_kb
+from bot.keyboard import main_kb, katalog_kb
 
 router = Router()
 json_data = "user_list.json"
 
+
 @router.message(F.text == "/start")
 async def start(message: Message):
     await message.answer("Добрый день", reply_markup=main_kb)
+
+
+@router.message(F.text == "Каталог")
+async def katalog(message: Message):
+    await message.answer("Каталог", reply_markup=katalog_kb)
--- bot/keyboard.py
+++ bot/keyboard.py
@@ -6,3 +6,8 @@ main = [
     [KeyboardButton(text="Настройки")]
 ]
 main_kb = ReplyKeyboardMarkup(keyboard=main, resize_keyboard=True)
+katalog = [
+    [InlineKeyboardButton(text="Клавиатуры", callback_data="Keyboards")],
+    [InlineKeyboardButton(text="Услуги", callback_data="Services")]
+]
+katalog_kb = InlineKeyboardMarkup(inline_keyboard=katalog)
