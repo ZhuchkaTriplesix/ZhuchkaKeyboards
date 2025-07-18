d269f7b042373245c480f3eddd3b2b8cfd90341c
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2025-07-18 21:43:55 +0300

    postgres cfg

--- gateway/src/config.py
+++ gateway/src/config.py
@@ -1,6 +1,7 @@
 import os
 from abc import ABC
 from dataclasses import asdict, dataclass
+from typing import Optional
 
 
 class CfgBase(ABC):
@@ -22,7 +23,17 @@ class JWTCfg(CfgBase):
     jwt_algorithm: str = os.getenv("JWT_ALGORITHM")
     jwt_exp: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
+class PostgresCfg(CfgBase):
+    host: str = os.getenv("POSTGRES_HOST")
+    port: str = os.getenv("POSTGRES_PORT")
+    user: str = os.getenv("POSTGRES_USER")
+    password: str = os.getenv("POSTGRES_PASSWORD")
+    db: str = os.getenv("POSTGRES_DB")
 
+    @property
+    def url(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{int(self.port)}/{self.db}"
 
 jwt_cfg = JWTCfg()
 redis_cfg = RedisCfg()
+postgres_cfg = PostgresCfg()
