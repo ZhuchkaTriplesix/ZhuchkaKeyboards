2ca183bb9d65079cc2904d9a4d06e733db8a73cc
ZhuchkaTriplesix <mrlololoshka94@gmail.com>
2025-07-20 09:31:11 +0300

    delete bad code

--- api/api.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from fastapi import APIRouter
-
-from .endpoints import auth
-
-api_router = APIRouter()
-
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
--- api/config.py
+++
@@ -1,1 +0,0 @@
-database = f'postgresql+pg8000://zhuchka:root@localhost:5432/postgres'
